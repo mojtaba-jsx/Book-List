18bc6c0e424b1434509184db602750ec982e6692
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-01-29 12:00:02 +0330

    add state for project

--- src/AddForm/AddForm.js
+++ src/AddForm/AddForm.js
@@ -1,21 +1,82 @@
-import React, { Component } from 'react'
-import './AddForm.css'
+import React, { Component } from "react";
+import "./AddForm.css";
+import Book from "../Book/Book";
 class AddForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: [1,2],
+      title: "",
+      author: "",
+      year: "",
+    };
+
+    this.titleHandler = this.titleHandler.bind(this);
+    this.authorHandler = this.authorHandler.bind(this);
+    this.yearHandler = this.yearHandler.bind(this);
+    this.submitHandler=this.submitHandler.bind(this)
+  }
+
+
+
+  titleHandler(event){
+    this.setState({
+      title:event.target.value
+    })
+  }
+
+
+  authorHandler(event){
+    this.setState({
+      author:event.target.value
+    })
+  }
+
+
+  yearHandler(event){
+    this.setState({
+      year:event.target.value
+    })
+  }
+
+  submitHandler(event){
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <div className='addform'>
-        <form className='form'>
+      <div className="addform">
+        <form className="form" onSubmit={this.submitHandler}>
           <label>عنوان کتاب</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={this.state.title}
+            onChange={this.titleHandler}
+          />
           <label>نویسنده</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={this.state.author}
+            onChange={this.authorHandler}
+          />
           <label>سال انتشار</label>
-          <input type="text" />
-          <button>اضافه کردن کتاب</button>
+          <input
+            type="text"
+            value={this.state.year}
+            onChange={this.yearHandler}
+          />
+          <button >اضافه کردن کتاب</button>
         </form>
+        <div className="book-list">
+          {
+            this.state.books.map((book)=>(
+              <Book title={book.title} author={book.author} year={book.year} />
+            ))
+          }
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default  AddForm
+export default AddForm;
--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "./Header/Header";
 import AddForm from "./AddForm/AddForm";
-import Book from "./Book/Book";
 class App extends Component {
   render() {
     return (
@@ -10,15 +9,7 @@ class App extends Component {
         <Header />
         <AddForm />
         <div className="container">
-          <div className="book-list">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-          </div>
+
         </div>
       </div>
     );
